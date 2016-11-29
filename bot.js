3427721a9d75d0ab546dd381c7cf62220aa48b7c
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-11-29 19:52:34 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,18 +5,18 @@
 const APP_ID = '07976ea0d7f1371a9e527add86391b84';
 
 function getCurrentWeather(location) {
-  return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APP_ID}`);
+  return get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APP_ID}`);
 }
 
 function getForecast(location) {
-  return axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&units=metric&cnt=5&APPID=${APP_ID}`);
+  return get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&units=metric&cnt=5&APPID=${APP_ID}`);
 }
 
 //export default function getWeather(location) {
 function getWeather(location) {
   const current = getCurrentWeather(location);
   const forecast = getForecast(location);
-  return axios.all([current, forecast])
+  return all([current, forecast])
     .then(function(results) {
       const currentWeather = processCurrentWeather(results);
       const forecastWeather = processForecastWeather(results);
