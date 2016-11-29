b221f99ab544b1e11704e4a45f15802bff4b8cbf
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-11-29 20:07:09 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,23 +5,17 @@
 const APP_ID = '07976ea0d7f1371a9e527add86391b84';
 
 function getCurrentWeather(location) {
-  return get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APP_ID}`);
+  return var current =(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APP_ID}`);
 }
 
 function getForecast(location) {
-  return get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&units=metric&cnt=5&APPID=${APP_ID}`);
+  return var forecast =(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&units=metric&cnt=5&APPID=${APP_ID}`);
 }
 
 //export default function getWeather(location) {
 function getWeather(location) {
   const current = getCurrentWeather(location);
   const forecast = getForecast(location);
-  return all([current, forecast])
-    .then(function(results) {
-      const currentWeather = processCurrentWeather(results);
-      const forecastWeather = processForecastWeather(results);
-      return [currentWeather, forecastWeather];
-    })
     .catch(function(error) {
       console.warn('Error in getWeather api', error);
     })
