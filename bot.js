14a92dfb507091d97fb2ae5ae24d4e402f8e770a
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-11-29 18:30:55 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,30 @@
 'use strict';
+import axios from 'axios';
+import { processCurrentWeather, processForecastWeather } from './helpers';
 
+const APP_ID = '1bd97c5c275dd5c9d80fba8c53c25549';
+
+function getCurrentWeather(location) {
+  return axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&APPID=${APP_ID}`);
+}
+
+function getForecast(location) {
+  return axios.get(`http://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&units=metric&cnt=5&APPID=${APP_ID}`);
+}
+
+export default function getWeather(location) {
+  const current = getCurrentWeather(location);
+  const forecast = getForecast(location);
+  return axios.all([current, forecast])
+    .then(function(results) {
+      const currentWeather = processCurrentWeather(results);
+      const forecastWeather = processForecastWeather(results);
+      return [currentWeather, forecastWeather];
+    })
+    .catch(function(error) {
+      console.warn('Error in getWeather api', error);
+    })
+}    
 // Weather Example
 // See https://wit.ai/sungkim/weather/stories and https://wit.ai/docs/quickstart
 const Wit = require('node-wit').Wit;
@@ -73,7 +98,7 @@ const actions = {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
     //context.forecast = getForecast(context.loc)
-    //context.forecast =getForecast(context.loc)
+    //context.forecast = getWeather(context.loc)
      context.forecast = 'Borzalmas idő van, DE!! Gödöllőn trópusi!';
     cb(context);
   },
