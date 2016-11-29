79cc9a8bf8d086cbcf84ee3270272c2b34579480
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-11-29 21:21:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -90,7 +90,7 @@ const actions = {
     // Here should go the api call, e.g.:
     // context.forecast = apiCall(context.loc)
     //context.forecast = getForecast(context.loc)
-    context.forecast = (`http://api.openweathermap.org/data/2.5/weather?q=Gödöllő&units=metric&APPID=07976ea0d7f1371a9e527add86391b84);
+    context.forecast = (`http://http://api.openweathermap.org/data/2.5/weather?q=${%27London%27}&units=metric&APPID=07976ea0d7f1371a9e527add86391b84');
     //context.forecast = 'Borzalmas idő van, DE!! Gödöllőn trópusi!';
     cb(context.forecast);
   },
