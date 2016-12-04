9ee79f758f9c058bbf08c8d77d0b19ea38ff32d1
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-12-04 12:47:54 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -78,7 +78,7 @@ const actions = {
   // getCurrent bot executes
   ['getCurrent'](sessionId, context, cb) {
     request({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${context.loc}&units=metric&type=accurate&APPID=07976ea0d7f1371a9e527add86391b84`,
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${context.loc}&units=metric&lang=hu&type=accurate&APPID=07976ea0d7f1371a9e527add86391b84`,
       json: true
     }, function (error, response, body) {
 
@@ -117,7 +117,7 @@ ${irany}
   // getForecast bot executes
   ['getForecast'](sessionId, context, cb) {
     request({
-      url: `http://api.openweathermap.org/data/2.5/weather?q=${context.loc}&units=metric&type=accurate&APPID=07976ea0d7f1371a9e527add86391b84`,
+      url: `http://api.openweathermap.org/data/2.5/weather?q=${context.loc}&units=metric&lang=hu&type=accurate&APPID=07976ea0d7f1371a9e527add86391b84`,
       json: true
     }, function (error, response, body) {
 
