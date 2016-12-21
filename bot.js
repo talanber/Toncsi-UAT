eb67a70f5f52b142f8286617c0dbd3aba50ddc3c
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-12-21 15:49:49 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,6 +55,50 @@ const monthMap = {
 };
 
 
+function toTitleCase(str) {
+  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+}
+
+function processDate(date = '') {
+  const dateInMilliseconds = new Date(date * 1000);
+  const day = dayMap[dateInMilliseconds.getDay()];
+  const month = `${monthMap[dateInMilliseconds.getMonth()]} ${dateInMilliseconds.getDate()}`;
+  return `${day}, ${month}`;
+}
+
+
+function processCurrentWeather(results) {
+  const current = results[0].data;
+  const date = processDate();
+  return {
+    date,
+    description: toTitleCase(current.weather[0].description),
+    icon: `/app/images/${current.weather[0].icon}.svg`,
+    temp: `${current.main.temp_min}°C`,
+    humidity: current.main.humidity
+  }
+}
+
+function processForecastWeather(results) {
+  const forecast = results[1].data;
+  let date;
+  let forecastWeather = [];
+  for (let i = 0; i < 5; i++) {
+    date =  processDate(forecast.list[i].dt);
+    forecastWeather.push({
+      date,
+      description: toTitleCase(forecast.list[i].weather[0].description),
+      icon: `/app/images/${forecast.list[i].weather[0].icon}.svg`,
+      minTemp: `${forecast.list[i].temp.min}°C`,
+      maxTemp: `${forecast.list[i].temp.max}°C`,
+      humidity: forecast.list[i].humidity
+    })
+  }
+  return forecastWeather;
+}  
+  
+  
+
 
 
 // Bot actions
@@ -146,50 +190,6 @@ ${irany}
   },
 // };
 
-
-function toTitleCase(str) {
-  return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-}
-
-function processDate(date = '') {
-  const dateInMilliseconds = new Date(date * 1000);
-  const day = dayMap[dateInMilliseconds.getDay()];
-  const month = `${monthMap[dateInMilliseconds.getMonth()]} ${dateInMilliseconds.getDate()}`;
-  return `${day}, ${month}`;
-}
-
-
-function processCurrentWeather(results) {
-  const current = results[0].data;
-  const date = processDate();
-  return {
-    date,
-    description: toTitleCase(current.weather[0].description),
-    icon: `/app/images/${current.weather[0].icon}.svg`,
-    temp: `${current.main.temp_min}°C`,
-    humidity: current.main.humidity
-  }
-}
-
-function processForecastWeather(results) {
-  const forecast = results[1].data;
-  let date;
-  let forecastWeather = [];
-  for (let i = 0; i < 5; i++) {
-    date =  processDate(forecast.list[i].dt);
-    forecastWeather.push({
-      date,
-      description: toTitleCase(forecast.list[i].weather[0].description),
-      icon: `/app/images/${forecast.list[i].weather[0].icon}.svg`,
-      minTemp: `${forecast.list[i].temp.min}°C`,
-      maxTemp: `${forecast.list[i].temp.max}°C`,
-      humidity: forecast.list[i].humidity
-    })
-  }
-  return forecastWeather;
-}  
-  
-  
   
   
   // getForecast bot executes
