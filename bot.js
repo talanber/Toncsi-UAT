270c9c01bd97b40433050f2b28c37da9969860da
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-12-21 22:37:30 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -209,13 +209,12 @@ ${irany}
 	var day = new Date();
 												 
 	for(q in response.list){
-		params.push(
 		{'daynum': day.getMonth()+1+"-"+day.getDate(),
  		 'temp':JSON.stringify(response.list[q].temp.min),
  		 'temp':JSON.stringify(response.list[q].temp.max),
 		 'hum':JSON.stringify(response.list[q].humidity),
 		 'des':response.list[q].weather[0].description,
-		 'icon':response.list[q].weather[0].icon});
+		 'icon':response.list[q].weather[0].icon};
 													  
 		 day.setDate(day.getDate()+1);
 	}
