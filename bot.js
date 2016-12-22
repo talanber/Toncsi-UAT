481ba71d7f941cf32962e0e6c7286782cd9e976e
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-12-22 15:36:32 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -150,6 +150,7 @@ Páratartalom         ${response.body.main.humidity} %
 A szélsebesség       ${response.body.wind.speed} km/óra
 ${irany}`
         cb(context);
+	context = {};
 
       }
     })
@@ -175,6 +176,7 @@ ${day.getMonth()+1+"."+day.getDate()} Min:${JSON.stringify(retData.list[q].temp.
 				day.setDate(day.getDate()+1);
 			}
         		cb(context);
+			context = {};
 		}
 		else{
 			console.log(err);
