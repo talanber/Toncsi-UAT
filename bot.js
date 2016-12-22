ddb1dad50454c0ddec52b74de7e00d52249d2916
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-12-22 11:01:28 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -222,9 +222,9 @@ ${day.getMonth()+1+"-"+day.getDate()}:Min: ${response.body.list[1].min}°C Max: 
 								day.setDate(day.getDate()+1);
 							}
 			
-						context1.city = retData.city.name+", "+retData.city.country;
-						context1.forecast = params;
-        					cb(context1);
+						context.city = retData.city.name+", "+retData.city.country;
+						context.forecast = params;
+        					cb(context);
 						//res.render('7Day',context1);
 					}
 					else{
