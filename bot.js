e110161798e67b869340fc477b86247e1717540b
Gazsi59 <gaspar.sandor.59@gmail.com>
2016-12-22 15:15:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -161,7 +161,7 @@ ${irany}`
 // getForecast bot executes
   ['getForecast'](sessionId, context, cb) {
 	   request({
-		 url: `http://api.openweathermap.org/data/2.5/forecast/daily?q=${context.loc}&units=metric&lang=hu&cnt=7&APPID=07976ea0d7f1371a9e527add86391b84`, get7Day);
+		 url: `http://api.openweathermap.org/data/2.5/forecast/daily?q=${context.loc}&units=metric&lang=hu&cnt=7&APPID=07976ea0d7f1371a9e527add86391b84`,
       json: true
     }, 	   function get7Day(err, response, body)
 	  {
